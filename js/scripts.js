33495834010f48f57739f698669c031e54c51e2e
ramzan <ramzanthenolph@gmail.com>
2020-05-10 20:54:50 +0300

    add css styling to portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
       $("#development-image").slideDown('1500');
     });
   });
-  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+      
