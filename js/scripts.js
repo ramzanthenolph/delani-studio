379cb3429443750636e6489a4f5b3390918bb3a2
ramzan <ramzanthenolph@gmail.com>
2020-05-10 21:08:26 +0300

    add jquery to form

--- js/scripts.js
+++ js/scripts.js
@@ -79,4 +79,21 @@ $(document).ready(function(){
     }).mouseout(function(){
       $("#overlay8").hide();
     });
-  });      
+  });
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
+        
